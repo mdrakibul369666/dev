78f157ffb4bebbcbb2318d8864b4c1cb37786151
mdrakibul369666 <143260241+mdrakibul369666@users.noreply.github.com>
2023-09-23 14:41:55 +0000

    new server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const fs = require('fs')
+import express from "express";
+import color from 'cli-color'
+const app = express()
+const port = process.env.PORT || '4000'
+app.listen(port, ()=>{
+    console.log(color.green(`server is running in ${port}`));
+})
+app.get('/' , (take , sent)=>{
+    sent.send('hellow node ')
+})
+
